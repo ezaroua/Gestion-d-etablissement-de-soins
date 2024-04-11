366f113f061dfbb77abd36fba4cb49148483672c
ezaroua <ezarouali.abdel@gmail.com>
2024-04-11 02:22:09 +0200

    Ajout de la fonction vider les champs sur le bouton vider

--- src/views/ViewAccueil.php
+++ src/views/ViewAccueil.php
@@ -6,89 +6,89 @@
     <link rel="stylesheet" href="static/css/styles.css">
 </head>
 <body>
+    <div class="navbar">
+        <a href="#home">Accueil</a>
+        <a href="#nursing"></a>
+        <a href="#ViewCreationPatient.php">Patient</a>
+        <!--TODO: Ajoutez d'autres liens de navigation-->
+    </div>
 
-<div class="navbar">
-    <a href="#home">Accueil</a>
-    <a href="#nursing"></a>
-    <a href="#patient">Patient</a>
-    <!-- Ajoutez d'autres liens de navigation si nécessaire -->
-</div>
+    <div class="main-header">
+        <h2>Bienvenue Ilias EZAROUALI</h2>
+    </div>
 
-<div class="main-header">
-    <h2>Bienvenue Ilias EZAROUALI</h2>
-    <!-- Formulaires et autres éléments d'interface utilisateur -->
-</div>
+    <div class="search-container">
+        <form id="patientForm" method="post">
+            <div class="row">
+                <div class="column">
+                    <label for="nom">Nom</label>
+                    <input type="text" id="nom" name="nom">
+                </div>
+                <div class="column">
+                    <label for="prenom">Prénom</label>
+                    <input type="text" id="prenom" name="prenom">
+                </div>
+                <div class="column">
+                    <label for="dateNaissance">Date de naissance</label>
+                    <input type="text" id="dateNaissance" name="dateNaissance">
+                </div>
+                <div class="column">
+                    <label for="idNat">N° secrurité social</label>
+                    <input type="text" id="idNat" name="idNat">
+                </div>
+            </div>
 
-<div class="search-container">
-    <form action="/path-to-your-script.php" method="post">
-        <div class="row">
-            <div class="column">
-                <label for="nom">Nom</label>
-                <input type="text" id="nom" name="nom">
-            </div>
-            <div class="column">
-                <label for="prenom">Prénom</label>
-                <input type="text" id="prenom" name="prenom">
-            </div>
-            <div class="column">
-                <label for="dateNaissance">Date de naissance</label>
-                <input type="text" id="dateNaissance" name="dateNaissance">
-            </div>
-            <div class="column">
-                <label for="idNat">N° secrurité social</label>
-                <input type="text" id="idNat" name="idNat">
-            </div>
-        </div>
-
-        <div class="row">
-            <div class="column">
-                <label for="patientId">Patient ID</label>
-                <input type="text" id="patientId" name="patientId">
-            </div>
-            <div class="column">
-                <label for="province">Province</label>
-                <select id="province" name="province">
-                    <option value="">Sélectionner...</option>
-                    <!-- Options de la province ici -->
-                </select>
-            </div>
-            <div class="column">
-                <label for="patientId">service</label>
-                <input type="text" id="patientId" name="patientId">
+            <div class="row">
+                <div class="column">
+                    <label for="patientId">Patient ID</label>
+                    <input type="text" id="patientId" name="patientId">
+                </div>
+                <div class="column">
+                    <label for="province">Province</label>
+                    <select id="province" name="province">
+                        <option value="">Sélectionner...</option>
+                        <!-- Options de la province ici -->
+                    </select>
+                </div>
+                <div class="column">
+                    <label for="patientId">service</label>
+                    <input type="text" id="patientId" name="patientId">
+                </div>
+                
             </div>
             
-        </div>
-        
-        <div class="center-buttons"> 
-            <button type="button">Chercher</button>
-            <button type="button">Vider</button>  
-        </div>
-    </form>
-</div>
+            <div class="center-buttons"> 
+                <button type="button">Chercher</button>
+                <button type="button" id="clearButton">Vider</button>  
+            </div>
+        </form>
+    </div>
 
-<table>
-    <tr>
-        <th>ID Personne</th>
-        <th>Patient</th>
-        <th>Date de naissance</th>
-        <th>Sexe</th>
-        <th>N° securité sociale</th>
-        <th>Medecin traitant</th>
-        
-    </tr>
-
-    <?php foreach($patients as $patient): ?>
+    <table>
         <tr>
-            <td><?= $patient->id_user() ?></td>
-            <td><?= $patient->nom(). ' ' .$patient->prenom()?> </td>
-            <td><?= $patient->date_naissance() ?></td>
-            <td><?= $patient->sexe() ?></td>
-            <td><?= $patient->num_sec() ?></td>
-            <td><?= $patient->MedecinTraitant() ?></td>
+            <th>ID Personne</th>
+            <th>Patient</th>
+            <th>Date de naissance</th>
+            <th>Sexe</th>
+            <th>N° securité sociale</th>
+            <th>Medecin traitant</th>
+            
         </tr>
-    <?php endforeach; ?>
 
-</table>
+        <?php foreach($patients as $patient): ?>
+            <tr>
+                <td><?= $patient->id_user() ?></td>
+                <td><?= $patient->nom(). ' ' .$patient->prenom()?> </td>
+                <td><?= $patient->date_naissance() ?></td>
+                <td><?= $patient->sexe() ?></td>
+                <td><?= $patient->num_sec() ?></td>
+                <td><?= $patient->MedecinTraitant() ?></td>
+            </tr>
+        <?php endforeach; ?>
+
+    </table>
+
+    <script src="static/js/script.js"></script>
 
 </body>
 </html>

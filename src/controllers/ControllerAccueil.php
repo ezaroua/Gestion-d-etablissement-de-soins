1a84190ff2c987e7b3ae1944c3e9074ebaf90393
thomas letoublon <tletoubgym74@gmail.com>
2024-04-16 09:55:52 +0200

    ajout patient et employe

--- src/controllers/ControllerAccueil.php
+++ src/controllers/ControllerAccueil.php
@@ -1,17 +1,20 @@
-<?php 
-class ControllerAccueil{
+<?php
+class ControllerAccueil
+{
 
-    private $_articleManager;
+    private $_patientManager;
     private $_view;
 
-    public function __construct($url){
-        if(isset($url) && is_array($url) && count($url) > 1)
+    public function __construct($url)
+    {
+        if (isset($url) && is_array($url) && count($url) > 1)
             throw new Exception('Page introuvable');
-        else 
-            $this->patients(); 
+        else
+            $this->patients();
     }
 
-    private function patients(){
+    private function patients()
+    {
         $this->_patientManager = new PatientManager;
         $patients = $this->_patientManager->getPatients();
 
